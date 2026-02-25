40df22987dd94cdb233a2e54950f6b96b82e23a3
meldameldamelda <melda.goh.2021@scis.smu.edu.sg>
2026-02-25 22:10:18 +0800

    troubleshooting wip

--- homefb.js
+++ homefb.js
@@ -17,13 +17,13 @@ onAuthStateChanged(auth, (user) => {
         const uid = user.uid;
         main_user = user
         console.log("user is signed in")
-        main.addEventListener("load", getAllDataOnce(main_user))
+        window.addEventListener("DOMContentLoaded", () => getAllDataOnce(main_user));
         // ...
     } else {
         // User is signed out
         // ...
         main_user = null
-        main.addEventListener("load", getAllDataOnce(main_user))
+        window.addEventListener("DOMContentLoaded", () => getAllDataOnce(main_user));
     }
 });
 
@@ -42,8 +42,6 @@ function getAllDataOnce(main_user) {
             `
         <div class="row row-cols-1 row-cols-lg-4 row-cols-md-3 row-cols-sm-2">
         `
-        // console.log(roomie)
-
         var roomieDiv = document.getElementById("roommates")
         for (var i = 0; i < roomie.length; i++) {
             // console.log(roomie[i])
@@ -63,9 +61,6 @@ function getAllDataOnce(main_user) {
             if (roomie[i].roomieImg) {
                 url = roomie[i].roomieImg
             }
-
-
-
 
             smth +=
                 `
@@ -101,8 +96,6 @@ function getAllDataOnce(main_user) {
 
         smth += `</div>`
         roomieDiv.innerHTML = smth
-        // console.log(roomieDiv)
-
     })
 
     get(child(dbRef, "property")).then((snapshot) => {
@@ -152,8 +145,6 @@ function getAllDataOnce(main_user) {
                 var wifiline = `<span class="badge bg-warning text-dark m-1"><i class="bi bi-wifi-off"></i></span>`
             }
 
-            // console.log(wificlass)
-
             str +=
                 `
             <div class="col project_container ${dNum}dollar ${wificlass} room${pBedroom} mb-3" >
@@ -196,8 +187,6 @@ function getAllDataOnce(main_user) {
         }
 
         str += "</div>"
-        // console.log(propertyDiv)
-
         propertyDiv.innerHTML = str
     })
     if (main_user != null) {
@@ -219,11 +208,8 @@ function getAllDataOnce(main_user) {
                         `
                 <div class="row row-cols-1 row-cols-lg-4 row-cols-md-3 row-cols-sm-2">
                 `
-                    // console.log(roomie)
-
                     var roomieDiv = document.getElementById("roommates")
                     for (var i = 0; i < roomie.length; i++) {
-                        // console.log(roomie[i])
                         let isChecked = ""
                         for (var j = 0; j < fav.length; j++) {
 
@@ -252,40 +238,39 @@ function getAllDataOnce(main_user) {
 
 
                         smth +=
-                            `
-                    <div class="col mb-3">
-                                <div class="card h-100" style='position:relative'  id="${listId}">
-                                    <input type="checkbox" id="heart${listId}" onchange="passValues(this)" ${isChecked}><label  for="heart${listId}" >&#9829</label></input>
-                                    <img class="img-fluid card-img-top" style="object-fit:cover; height:200px" src=${url} alt="project-img">
-                                    <div class="card-body">
-                                        <a href="/roomieListing/roomieListing.html?listId=${listId}" id = "${listId}">
-                                            <h5 class="card-title text-success fw-bolder">${rName}, ${rAge}</h5>
-                                        </a>
-                                        <div class="card-text d-flex pb-2">
-                                            <span><i class="bi bi-geo-alt-fill" ></i></span>
-                                            <div class="fw-light fs-6 px-2">${rLoc}</div>
-                                        </div>
-                                        <div class="card-text d-flex pb-2">
-                                            <span><i class="bi bi-currency-dollar"></i></span>
-                                            <div class="fw-light fs-6 px-2">${rBudget} / month</div>
-                                        </div>
-                                        <div class="card-text d-flex pb-2">
-                                            <span>              
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-clock-fill mb-1 me-1" viewBox="0 0 16 16">
-                                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
-                                                </svg>
-                                            </span>
-                                            <div class="fw-light fs-6 px-2">${rDuration} month(s)</div>
+                        `
+                        <div class="col mb-3">
+                                    <div class="card h-100" style='position:relative'  id="${listId}">
+                                        <input type="checkbox" id="heart${listId}" onchange="passValues(this)" ${isChecked}><label  for="heart${listId}" >&#9829</label></input>
+                                        <img class="img-fluid card-img-top" style="object-fit:cover; height:200px" src=${url} alt="project-img">
+                                        <div class="card-body">
+                                            <a href="/roomieListing/roomieListing.html?listId=${listId}" id = "${listId}">
+                                                <h5 class="card-title text-success fw-bolder">${rName}, ${rAge}</h5>
+                                            </a>
+                                            <div class="card-text d-flex pb-2">
+                                                <span><i class="bi bi-geo-alt-fill" ></i></span>
+                                                <div class="fw-light fs-6 px-2">${rLoc}</div>
+                                            </div>
+                                            <div class="card-text d-flex pb-2">
+                                                <span><i class="bi bi-currency-dollar"></i></span>
+                                                <div class="fw-light fs-6 px-2">${rBudget} / month</div>
+                                            </div>
+                                            <div class="card-text d-flex pb-2">
+                                                <span>              
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-clock-fill mb-1 me-1" viewBox="0 0 16 16">
+                                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
+                                                    </svg>
+                                                </span>
+                                                <div class="fw-light fs-6 px-2">${rDuration} month(s)</div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
                     `
                     }
 
                     smth += `</div>`
                     roomieDiv.innerHTML = smth
-                    // console.log(roomieDiv)
 
                 })
                 get(child(dbRef, "property")).then((snapshot) => {
@@ -298,7 +283,6 @@ function getAllDataOnce(main_user) {
                     var str = `<div class="row row-cols-1 row-cols-lg-4 row-cols-md-3 row-cols-sm-2">`
 
                     for (var i = 0; i < property.length; i++) {
-                        // console.log(property[i])
                         let isChecked2 = ""
                         for (var j = 0; j < fav.length; j++) {
                             if (fav[j] == property[i].listId) {
@@ -383,8 +367,6 @@ function getAllDataOnce(main_user) {
 
 
                     str += "</div>"
-                    // console.log(propertyDiv)
-
                     propertyDiv.innerHTML = str
                 })
             }
@@ -399,8 +381,6 @@ function getAllDataOnce(main_user) {
                         `
         <div class="row row-cols-1 row-cols-lg-4 row-cols-md-3 row-cols-sm-2">
         `
-                    // console.log(roomie)
-
                     var roomieDiv = document.getElementById("roommates")
                     for (var i = 0; i < roomie.length; i++) {
                         // console.log(roomie[i])
@@ -420,9 +400,6 @@ function getAllDataOnce(main_user) {
                         if (roomie[i].roomieImg) {
                             url = roomie[i].roomieImg
                         }
-
-
-
 
                         smth +=
                         `
@@ -458,7 +435,6 @@ function getAllDataOnce(main_user) {
 
                     smth += `</div>`
                     roomieDiv.innerHTML = smth
-                    // console.log(roomieDiv)
 
                 })
 
@@ -472,7 +448,6 @@ function getAllDataOnce(main_user) {
                     var str = `<div class="row row-cols-1 row-cols-lg-4 row-cols-md-3 row-cols-sm-2">`
 
                     for (var i = 0; i < property.length; i++) {
-                        // console.log(property[i])
                         const pInternet = property[i].internet
 
                         const pTitle = property[i].title
@@ -486,7 +461,6 @@ function getAllDataOnce(main_user) {
                             url = property[i].propertyImg
                         }
 
-                        // console.log(parseInt(pRent))
                         if ((parseInt(pRent)) < 750) {
                             var dNum = "one"
                             var dSign = "$"
@@ -510,9 +484,6 @@ function getAllDataOnce(main_user) {
                             var wificlass = "nowifi"
                             var wifiline = `<span class="badge bg-warning text-dark m-1"><i class="bi bi-wifi-off"></i></span>`
                         }
-
-                        // console.log(dNum)
-                        // console.log(wificlass)
 
                         str +=
                             `
@@ -559,8 +530,6 @@ function getAllDataOnce(main_user) {
                     propertyDiv.innerHTML = str
                 })
             }
-
-
         })
     }
 }
