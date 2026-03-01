c3f9f24f1bc4bf3a2d67490b79b67b36f819230e
meldameldamelda <melda.goh.2021@scis.smu.edu.sg>
2026-03-01 17:41:19 +0800

    - minor cosmetic adjustments - cleaned up register/login files

--- navComponent/navComponent.js
+++ navComponent/navComponent.js
@@ -4,23 +4,15 @@ const main = Vue.createApp({});
 // Create a new Vue component
 main.component("nav-component", {
   template: `
-        <nav class="navbar navbar-expand-xl fixed-top w-100" style='background-color:#fbaee3; background-image: url("/images/navBarBg.png"); background-size: 520px; background-repeat: no-repeat; background-position: right; background-position-y: 0px;'>
-          <a class="navbar-brand" href="../home.html">
-            <img class='mx-4'src='/images/logo.png' style='width: 150px;'>
-          </a>
-        
-          <div class="navbar-nav id="navbarSupportedContent">
-            <ul class="navbar-nav">
-              <li class="nav-item mx-4">
-                <a class="nav-link" href="/profile/profile.html">
-                <br>
-                <h1 class="fs-5 fw-bold" style="color: #0b60f6;">Profile</a></h1>
-              </li>
-=            </ul>
-          </div>
-      </nav> 
-      
-  </body>
+    <nav class="navbar navbar-expand-xl fixed-top w-100 flex justify-content-between" style='background-color:#fbaee3; background-image: url("/images/navBarBg.png"); background-size: 520px; background-repeat: no-repeat; background-position: 180px; background-position-y: 0px;'>
+      <a class="navbar-brand" href="../home.html">
+        <img class='mx-4'src='/images/logo.png' style='width: 150px;'>
+      </a>
+    
+      <a class="nav-link mx-4" href="/profile/profile.html">
+        <img src="/images/profile.png" style="height: 60px; margin-right: 5px;">
+      </a>
+    </nav>
   `,
 });
 
