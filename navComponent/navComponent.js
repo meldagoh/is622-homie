ee7d0075807ab467cf8cff5f41c43399552e33b5
meldameldamelda <melda.goh.2021@scis.smu.edu.sg>
2026-03-02 18:47:47 +0800

    navComponent - replaced home.html link in logo with loading.html

--- navComponent/navComponent.js
+++ navComponent/navComponent.js
@@ -5,7 +5,7 @@ const main = Vue.createApp({});
 main.component("nav-component", {
   template: `
     <nav class="navbar navbar-expand-xl fixed-top w-100 flex justify-content-between" style='background-color:#fbaee3; background-image: url("/images/navBarBg.png"); background-size: 520px; background-repeat: no-repeat; background-position: 180px; background-position-y: 0px;'>
-      <a class="navbar-brand" href="../home.html">
+      <a class="navbar-brand" href="../loading/loading.html">
         <img class='mx-4'src='/images/logo.png' style='width: 150px;'>
       </a>
     
